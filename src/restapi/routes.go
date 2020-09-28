0a862ce0ae91b763147929c265c419525ae2e2fd
imjoseangel <josea.munoz@gmail.com>
2020-09-28 20:54:39 +0200

    Add router

--- src/restapi/routes.go
+++ src/restapi/routes.go
@@ -3,8 +3,6 @@ package main
 
 import (
 	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
 // Route ...
@@ -17,27 +15,6 @@ type Route struct {
 
 // Routes ...
 type Routes []Route
-
-// NewRouter ...
-func NewRouter() *mux.Router {
-
-	router := mux.NewRouter().StrictSlash(true)
-	for _, route := range routes {
-
-		var handler http.Handler
-
-		handler = route.HandlerFunc
-		handler = Logger(handler, route.Name)
-
-		router.
-			Methods(route.Method).
-			Path(route.Pattern).
-			Name(route.Name).
-			Handler(handler)
-	}
-
-	return router
-}
 
 var routes = Routes{
 	Route{
