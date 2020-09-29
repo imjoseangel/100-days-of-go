96fd1665b051b7f07a6de633d008424189798f22
imjoseangel <josea.munoz@gmail.com>
2020-09-29 08:52:54 +0200

    Add API Read

---
+++ src/spacecount/main.go
@@ -0,0 +1,54 @@
+///bin/true; exec /usr/bin/env go run "" "$@"
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"time"
+)
+
+type people struct {
+	Number int `json:"number"`
+}
+
+func main() {
+
+	url := "http://api.open-notify.org/astros.json"
+
+	// text := `{"number": 3, "people": [{"craft": "ISS", "name": "Chris Cassidy"}, {"craft": "ISS", "name": "Anatoly Ivanishin"}, {"craft": "ISS", "name": "Ivan Vagner"}], "message": "success"}`
+	spaceClient := http.Client{
+		Timeout: time.Second * 2, // Timeout after 2 seconds
+	}
+
+	req, err := http.NewRequest(http.MethodGet, url, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+	req.Header.Set("User-Agent", "Mozilla/5.0 (X11; Linux x86_64)")
+
+	res, getErr := spaceClient.Do(req)
+	if getErr != nil {
+		log.Fatal(getErr)
+	}
+
+	if res.Body != nil {
+		defer res.Body.Close()
+	}
+
+	body, readErr := ioutil.ReadAll(res.Body)
+	if readErr != nil {
+		log.Fatal(readErr)
+	}
+
+	persons := people{}
+	jsonErr := json.Unmarshal(body, &persons)
+
+	if jsonErr != nil {
+		log.Fatal(jsonErr)
+	}
+
+	fmt.Println(persons.Number)
+}
